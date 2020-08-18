c1a24479258d31d0453511bcc3bb639a73471609
willyaditya <willyaditya03@gmail.com>
2020-08-18 10:28:11 +0700

    menambahkan function stack

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 alert("uwu");
+
+function Stack() {
+    this.size = 0;
+    this.data = [];
+
+    this.pushStack = function (data) {
+        var size = this.size++;
+        this.data[size] = data;
+        return this.data[size];
+    }
+
+    this.popStack = function () {
+        var size = this.size, lastData;
+
+        if (size) {
+            lastData = this.data[size - 1];
+            this.data.length = size - 1;
+            this.size--;
+
+            return lastData;
+        } else {
+            return false;
+        }
+    }
+
+    this.allStack = function () {
+        return this.data;
+    }
+};
